b37743a6b2e6d5560bbcd908e213e4088f8c6dcb
gustavobmelo <168488474+gustavobmelo@users.noreply.github.com>
2025-08-28 01:09:32 +0000

    aula 28/08

--- aula01exer01/schema.sql
+++ aula01exer01/schema.sql
@@ -1,4 +1,4 @@
--- exemplo de criação de um banco de dados, comando: sqlite3 loja.db
+-- exemplo de criação de um banco de dados, comando: ls // depois: sqlite3 mais o banco de dados (loja.db)
 -- comando: PRAGMA foreign_keys = NO; + alt shift v
 -- comando: sqlite3 loja.db + alt shift v
 -- COPIAR A TABELA E COLAR NO TERMINAL ANTES DE EXECUTAR O COMANDO NO TERMINAL
@@ -52,3 +52,33 @@ CREATE TABLE venda_produto(
     FOREIGN KEY (venda_id) REFERENCES venda(id) ON DELETE CASCADE,
     FOREIGN KEY (produto_id) REFERENCES produto(id) ON DELETE CASCADE
 );
+
+--Inserir usuario
+INSERT INTO usuario (nome, email, senha) VALUES ('Rafael','rafael@unipam','leafar');
+INSERT INTO usuario (nome, email, senha) VALUES ('Junior','junior@unipam','leafar');
+INSERT INTO usuario (nome, email, senha) VALUES ('James','james@unipam','leafar');
+INSERT INTO usuario (nome, email, senha) VALUES ('Steve','steve@unipam','leafar');
+
+--Inserir cliente
+INSERT INTO cliente (nome, telefone, usuario_id) VALUES ('Rafael Marinho','999999999', 1);
+INSERT INTO cliente (nome, telefone, usuario_id) VALUES ('Junior Savior','988888888',2);
+INSERT INTO cliente (nome, telefone, usuario_id) VALUES ('James Stafe','977777777',3);
+INSERT INTO cliente (nome, telefone, usuario_id) VALUES ('Steve Henri','966666666',4);
+
+--Inserir produtos
+INSERT INTO produto (nome, descricao, preco) VALUES ('Arroz 5kg','Arroz tipo 1', 10.20);
+INSERT INTO produto (nome, descricao, preco) VALUES ('Feijao 1kg','Feijao carioca', 7.25);
+INSERT INTO produto (nome, descricao, preco) VALUES ('Acucar 2kg','Acucar refinado', 5.35);
+
+--Inserir venda
+INSERT INTO venda (data, usuario_id, cliente_id) VALUES ('2025-08-27', 1, 1);
+INSERT INTO venda (data, usuario_id, cliente_id) VALUES ('2025-08-27', 2, 2);
+INSERT INTO venda (data, usuario_id, cliente_id) VALUES ('2025-08-27', 3, 3);
+
+--Inserir dados na tabela de associação venda_produto
+INSERT INTO venda_produto (venda_id, produto_id, quantidade, preco_unitario) VALUES (1,1,3,10.20);
+INSERT INTO venda_produto (venda_id, produto_id, quantidade, preco_unitario) VALUES (1,2,5,7.25);
+INSERT INTO venda_produto (venda_id, produto_id, quantidade, preco_unitario) VALUES (1,2,5,7.25);
+INSERT INTO venda_produto (venda_id, produto_id, quantidade, preco_unitario) VALUES (2,2,1,7.25);
+INSERT INTO venda_produto (venda_id, produto_id, quantidade, preco_unitario) VALUES (2,2,1,7.25);
+INSERT INTO venda_produto (venda_id, produto_id, quantidade, preco_unitario) VALUES (3,1,2,10.20);
